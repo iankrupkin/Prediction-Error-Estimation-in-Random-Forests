9fd183845862ec17952e24e4a2a6ef1a1727189c
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-02-17 11:18:54 -0800

    Made second big-func and ianrun

---
+++ big-func2.R
@@ -0,0 +1,30 @@
+library(tidymodels)
+library(tidyverse)
+library(tuneRanger)
+library(furrr)
+library(future)
+library(tictoc)
+
+source("fdt.R")
+source("fdo.R")
+source("sdt.R")
+source("sdo.R")
+source("sdcv.R")
+
+plan(multisession, workers = 100)
+
+n <- 20
+p <- 10
+prop <- 0.5
+reps <- 1000
+strat <- "fdo"
+
+start_time <- Sys.time()
+data <- future_map_dfr(1:reps,fdo)
+end_time <- Sys.time()
+
+sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
+print(end_time - start_time)
+sink()
+
+write.csv(data, paste0(paste(paste(strat,n,p,prop, sep = "_")),".csv"))
---
+++ ianrun2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH --cpus-per-task=50
+#SBATCH --mem=500G
+#SBATCH --time=1-00:15:00     # 1 day and 15 minutes
+#SBATCH --mail-user=email@address.com
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="just_a_test"
+#SBATCH -p amd # This is the default partition
+#SBATCH --output=log_filename.log #if you do not use this, it will create a slurm-[jobid].out as the log
+
+#your other command down here
+module load r
+
+
+R CMD BATCH big-func2.R
