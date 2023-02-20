da3f2c00008de9d02965c1e2be07660ceb243837
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-02-20 10:44:28 -0800

    Changed mem

--- ianrun2.sh
+++ ianrun2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH --cpus-per-task=50
-#SBATCH --mem=500G
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=250G
 #SBATCH --time=1-00:15:00     # 1 day and 15 minutes
 #SBATCH --mail-user=email@address.com
 #SBATCH --mail-type=ALL
-#SBATCH --job-name="just_a_test"
+#SBATCH --job-name="big-func2"
 #SBATCH -p amd # This is the default partition
 #SBATCH --output=log_filename.log #if you do not use this, it will create a slurm-[jobid].out as the log
 
