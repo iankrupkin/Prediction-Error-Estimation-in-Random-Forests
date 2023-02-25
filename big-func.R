ce5fe74f6fb4499ac654b4fd1f83afa7dd97eb97
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-02-25 10:06:30 -0800

    Changed parameters

--- big-func.R
+++ big-func.R
@@ -12,15 +12,15 @@ source("newsdcv.R")
 
 plan(multisession, workers = 100)
 
-n <- 100
+n <- 50
 p <- 1000
-prop <- 0.7
+prop <- 0.83
 reps <- 1000
 n.holdout <- 20000
-strat <- "sdt"
+strat <- "fdo"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,sdt) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
+data <- future_map_dfr(1:reps,fdo) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
