04c01dfc3d61befecfd7376cf52e02c6a4a1ca75
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-02-28 19:09:22 -0800

    Starting sdo n=500, prop=0.5

--- big-func.R
+++ big-func.R
@@ -14,14 +14,14 @@ source("newfdcv.R")
 plan(multisession, workers = 100)
 
 n <- 500
-p <- 1000
+p <- 10
 prop <- 0.83
 reps <- 1000
 n.holdout <- 20000
-strat <- "sdt"
+strat <- "sdo"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,sdt) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
+data <- future_map_dfr(1:reps,sdo) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
