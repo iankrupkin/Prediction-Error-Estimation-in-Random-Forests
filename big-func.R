a1286f9b5f64a3fb636245dc0f77fb1c0b45f88f
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-18 12:21:57 -0700

    Trying fdcv again

--- big-func.R
+++ big-func.R
@@ -13,15 +13,15 @@ source("newfdcv.R")
 
 plan(multisession, workers = 100)
 
-n <- 250
+n <- 100
 p <- 10
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "fdo"
+strat <- "fdcv"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,fdo) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
+data <- future_map_dfr(1:reps,fdcv) %>% mutate(err = mean(errxy), err_fpr = mean(errxy_fpr), err_fnr = mean(errxy_fnr))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
