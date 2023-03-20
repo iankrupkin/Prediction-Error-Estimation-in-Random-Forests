a8723f913bc0c9feb11f37c0e087ed3278688ab1
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-20 16:14:22 -0700

    fdcv with log reg p=100, n=2000,2500

--- big-func.R
+++ big-func.R
@@ -15,7 +15,7 @@ source("fdcvlogreg.R")
 
 plan(multisession, workers = 100)
 
-n <- 1500
+n <- 2000
 p <- 100
 prop <- 0.5
 reps <- 1000
--- big-func2.R
+++ big-func2.R
@@ -15,7 +15,7 @@ source("fdcvlogreg.R")
 
 plan(multisession, workers = 100)
 
-n <- 1250
+n <- 2500
 p <- 100
 prop <- 0.5
 reps <- 1000
