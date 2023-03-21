3f5aff6be6d4a80f6fde8b64882420f39c4a3620
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-21 12:32:54 -0700

    fdcv rf p=10,n=10,25

--- newfdcv.R
+++ newfdcv.R
@@ -26,8 +26,8 @@ fdcv <- function(x){
 }
 
 fdcv2 <- function(x){
-  data <- data.frame(pmap(list(n,p,prop),data.generation))
-  ho.data <- data.frame(pmap(list(n.holdout,p,prop),data.generation))
+  data <- data.frame(pmap(list(n,p,prop),power.data.2))
+  ho.data <- data.frame(pmap(list(n.holdout,p,prop),power.data.2))
   train.control <- trainControl(method = "cv", number = 4)
   model <- train(class~., data = data, method = "rf", tuneGrid=data.frame(mtry=sqrt(p)), trees = 500, min_n = 5, trControl = train.control)
   err.hat <- 1-model[["results"]][["Accuracy"]]
