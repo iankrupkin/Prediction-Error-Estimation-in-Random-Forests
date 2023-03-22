25d7cf99b22c349356f6347666ba53918a10653a
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-22 11:24:41 -0700

    sdt p=10, n=10,25

--- big-func.R
+++ big-func.R
@@ -15,15 +15,15 @@ source("fdcvlogreg.R")
 
 plan(multisession, workers = 100)
 
-n <- 750
-p <- 100
+n <- 10
+p <- 10
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "sdcv"
+strat <- "sdt"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,sdcv) %>% mutate(err = mean(errxy, na.rm=TRUE))
+data <- future_map_dfr(1:reps,sdt) %>% mutate(err = mean(errxy, na.rm=TRUE))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
