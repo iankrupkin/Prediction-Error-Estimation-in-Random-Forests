521aa5ac280f4a02ca09c2dc924f7cb328a5d178
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-27 09:32:59 -0700

    New n/p combos fdo

--- big-func2.R
+++ big-func2.R
@@ -15,15 +15,15 @@ source("fdcvlogreg.R")
 
 plan(multisession, workers = 100)
 
-n <- 75
-p <- 10
+n <- 25
+p <- 100
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "sdt"
+strat <- "fdo"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,sdt) %>% mutate(err = mean(errxy, na.rm=TRUE))
+data <- future_map_dfr(1:reps,fdo) %>% mutate(err = mean(errxy, na.rm=TRUE))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
