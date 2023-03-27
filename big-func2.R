6eda8ebe25e82dcd44a7c28a7e1d18ca41bf34ea
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-03-27 10:30:18 -0700

    new n/p combos fdcv2

--- big-func2.R
+++ big-func2.R
@@ -20,10 +20,10 @@ p <- 10
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "fdcvlogreg"
+strat <- "fdcv2"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,fdcvlogreg) %>% mutate(err = mean(errxy, na.rm=TRUE))
+data <- future_map_dfr(1:reps,fdcv2) %>% mutate(err = mean(errxy, na.rm=TRUE))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,".csv"))
