52c1df50b3f66ad7b086b70834295aabc2f1bf1e
ikaa2018 <ikaa2018@rstudio01p.campus.pomona.edu>
2023-04-17 10:20:31 -0700

    fdcv bates nps

--- big-func.R
+++ big-func.R
@@ -6,24 +6,24 @@ library(future)
 library(tictoc)
 library(caret)
 
-source("newfdo.R")
-source("newsdt.R")
-source("newsdo.R")
-source("newsdcv.R")
-source("newfdcv.R")
-source("fdcvlogreg.R")
+source("fdo.R")
+source("sdt.R")
+source("sdo.R")
+source("sdcv.R")
+source("fdcv.R")
+source("lgcv.R")
 
 plan(multisession, workers = 100)
 
-n <- 1120
+n <- 50
 p <- round(n/5)
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "fdcvlogreg"
+strat <- "fdcv"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,fdcvlogreg) %>% mutate(err = mean(errxy, na.rm=TRUE))
+data <- future_map_dfr(1:reps,fdcv) %>% mutate(err = mean(errxy, na.rm=TRUE))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,"_",reps,".csv"))
--- big-func2.R
+++ big-func2.R
@@ -15,15 +15,15 @@ source("lgcv.R")
 
 plan(multisession, workers = 100)
 
-n <- 800
+n <- 100
 p <- round(n/5)
 prop <- 0.5
 reps <- 1000
 n.holdout <- 20000
-strat <- "fdcvlogreg"
+strat <- "fdcv"
 
 start_time <- Sys.time()
-data <- future_map_dfr(1:reps,fdcvlogreg) %>% mutate(err = mean(errxy, na.rm=TRUE))
+data <- future_map_dfr(1:reps,fdcv) %>% mutate(err = mean(errxy, na.rm=TRUE))
 end_time <- Sys.time()
 
 sink(paste0("time_",strat,"_",n,"_",p,"_",prop,"_",reps,".csv"))
